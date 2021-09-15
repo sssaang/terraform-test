b452270ba954d60d7cb23c4bdc4b248e6b45ee5c
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-09-15 22:01:27 +0900

    autoscaling group

--- my-first-terraform/main.tf
+++ my-first-terraform/main.tf
@@ -18,6 +18,37 @@ resource "aws_instance" "test" {
   }
 }
 
+resource "aws_launch_configuration" "test" {
+  image_id = "ami-0ba5cd124d7a79612"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World!" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_autoscaling_group" "test" {
+  launch_configuration = aws_launch_configuration.test.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
 resource "aws_security_group" "instance"{
   name = "terraform-example-instance"
 
@@ -38,4 +69,12 @@ variable "server_port" {
 output "public_ip" {
   value = aws_instance.test.public_ip
   description = "The public ip of the web server"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
